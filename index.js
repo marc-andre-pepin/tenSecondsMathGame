9242ffe34a4a5fb92c30038d1c3a63bc76c9e2fa
marc-andre-pepin <109531811+marc-andre-pepin@users.noreply.github.com>
2022-12-08 12:29:46 -0600

    Add bonus features (leaderboard, custom max operand value)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     var currentQuestion;
     var interval;
     var timeLeft = 10;
     var score = 0;
     var maxScore = 50;
-    var maxOperand = 10;
     var maxTime = 10;
+    var topScores = [];
 
     var randomNumberGenerator = function (size) {
         return Math.ceil(Math.random() * size);
@@ -14,6 +14,7 @@ $(document).ready(function(){
 
     var questionGenerator = function () {
         var question = {};
+        const maxOperand = $('#maxNumber').val()
         var num1 = randomNumberGenerator(maxOperand);
         var num2 = randomNumberGenerator(maxOperand);
 
@@ -33,7 +34,7 @@ $(document).ready(function(){
     }
 
     var checkAnswer = function (userInput, answer) {
-        if(userInput === answer) {
+        if (userInput === answer) {
             renderNewQuestion();
             $('#user-input').val('');
             updateTimeLeft(+1);
@@ -43,14 +44,14 @@ $(document).ready(function(){
 
     var updateTimeLeft = function (amount) {
         timeLeft += amount;
-        if(timeLeft > maxTime) {
+        if (timeLeft > maxTime) {
             timeLeft = maxTime
         }
         $('#time-left').text(timeLeft);
         $('#time-left-label').text(timeLeft === 1 ? 'second left' : 'seconds left');
     }
 
-    var startGame = function () {
+    var handleInput = function () {
         if (!interval) {
             if (timeLeft === 0) {
                 updateTimeLeft(maxTime);
@@ -65,24 +66,62 @@ $(document).ready(function(){
         }
     }
 
+    var startGame = function () {
+        clearInterval(interval);
+        interval = undefined;
+
+        var userInput = $('#user-input')
+        userInput.val('');
+        userInput.prop('disabled', false);
+
+        score = 0;
+        $('#score').text(score);
+
+        timeLeft = 10;
+        $('#time-left').text(timeLeft);
+        $('#time-left-label').text('seconds left');
+
+        renderNewQuestion();
+    }
+
+    $('#startGame').click(startGame)
+
+    var updateLeaderboard = function () {
+        topScores.push(score);
+        topScores.sort(function (a, b) {
+            return b - a;
+        });
+        if (topScores.length > 5) {
+            topScores.pop();
+        }
+
+        var leaderboard = $('#leaderboard');
+        leaderboard.empty();
+        for (var topScore of topScores) {
+            var li = $(`<li>${topScore}</li>`);
+            leaderboard.append(li);
+        }
+    }
+
     var endGame = function () {
         clearInterval(interval);
         interval = undefined;
         $('#user-input').prop('disabled', true);
         $('#time-left').text(score >= maxScore ? 'Amazing!' : 'Time\'s up!');
         $('#time-left-label').text(score >= maxScore ? 'You reached 50!' : `Your score was ${score}`);
+        updateLeaderboard();
     }
 
     var updateScore = function (amount) {
         score += amount;
         $('#score').text(score);
-        if(score >= maxScore) {
+        if (score >= maxScore) {
             endGame();
         }
     };
 
     $('#user-input').on('keyup', function () {
-        startGame();
+        handleInput();
         checkAnswer(Number($(this).val()), currentQuestion.answer);
     });
 
